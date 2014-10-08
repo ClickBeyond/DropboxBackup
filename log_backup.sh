8bfcf36ef3567ac0b110001ca65b6aeb731be474
Daniel Newman <dan@cubicapps.com>
2014-10-08 22:40:49 +0100

    Added a script to backup the log file

---
+++ log_backup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+###################################################
+# A script to backup your log file to dropbox.
+#
+# Author: Dan @ CubicApps
+# Website: https://github.com/CubicApps
+# Copyright 2014
+#
+# Usage: $ ./log_backup.sh
+#
+# Before running this script for the first time make 
+# sure you have already run the `./db_backup.sh` script. 
+# Also make sure you give this script execution 
+# permissions:
+# $ chmod +x log_backup.sh
+#
+###################################################
+
+echo "Starting backup of 'backup.log' to dropbox..."
+start=$SECONDS
+cd ~/tmp
+BKP_LOG_FILE="log-backup-$(date +"%Y-%m-%d_%H-%M-%S").tar.gz"
+tar -zcf "$BKP_LOG_FILE" "backup.log"
+~/DropboxBackup/dropbox_uploader.sh -f ~/.dropbox_uploader upload $BKP_LOG_FILE "/Log_Backups/$BKP_LOG_FILE"
+rm $BKP_LOG_FILE
+duration=$(( SECONDS - start ))
+echo "Log backup complete! Finished in $duration seconds!"
